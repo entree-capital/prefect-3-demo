861e8eaa19f2c0fda8e167e2f521b321bb2db0b2
Pavel 'PK' Kaminsky <kaminsky.pavel@gmail.com>
2024-07-17 18:25:54 +0300

    chore: Remove deprecated AWS worker flow deployment code

--- flows/aws-worker.py
+++ flows/aws-worker.py
@@ -4,17 +4,18 @@ from prefect.docker import DockerImage
 
 @flow(log_prints=True)
 def my_flow(name: str = "world"):
-    print(f"Hello {name}! I'm a flow AWS ✨✨✨✨✨✨✨✨")
+    print(f"Hello {name}! I'm a flow AWS 🤔😆😆😆😆😆😆😆✅✅✅")
 
 
 if __name__ == "__main__":
-    my_flow.deploy(
-        name="my-deployment1",
-        work_pool_name="my-ecs-pool",
-        image=DockerImage(
-            name="kaminskypavel/prefect-3-demo",
-            tag="latest",
-            dockerfile="Dockerfile",
-            platform="linux/amd64",
-        ),
-    )
+    # this is deprecated, use `prefect deploy` instead
+    # my_flow.deploy(
+    #     name="my-deployment1",
+    #     work_pool_name="my-ecs-pool",
+    #     image=DockerImage(
+    #         name="docker.io/entreecapital/prefect-3-demo",
+    #         tag="latest",
+    #         dockerfile="Dockerfile",
+    #         platform="linux/amd64",
+    #     ),
+    # )
